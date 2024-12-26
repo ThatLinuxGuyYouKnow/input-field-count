276f555e37a31df78104206ec899e7e62eb48503
Emmanuels-code <arocket04@gmail.com>
2024-12-26 16:59:41 +0100

    updated content.js

--- scripts/content.js
+++ scripts/content.js
@@ -2,8 +2,9 @@
 function checkInputFields() {
     const inputFields = document.querySelectorAll('input, textarea, select');
     // return the number of input fields on a webpage
+    console.log('found ' + inputFields.length + 'fields')
     return inputFields.length;
 }
 
-// Send a message to the background script to check for input fields
+
 chrome.runtime.sendMessage({ action: 'checkInputFields', inputFieldCount: checkInputFields() });
