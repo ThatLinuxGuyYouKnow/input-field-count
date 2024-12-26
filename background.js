1392b12e1ef0786ff44101b02733da4ca31ebdd2
Emmanuels-code <arocket04@gmail.com>
2024-12-26 16:56:08 +0100

    updated background.js

--- background.js
+++ background.js
@@ -1,10 +1,30 @@
 // background.js
+chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
 
-// ... (rest of your background script code like tab listeners and sidePanel setup)
+// send checkInputFields message on tab activation
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    executeCheckInputFields(activeInfo.tabId);
+});
+// send checkInputFields message on tab update
+chrome.tabs.onUpdated.addListener(async (tabId) => {
+    executeCheckInputFields(tabId);
+});
+
+async function executeCheckInputFields(tabId) {
+    const tab = await chrome.tabs.get(tabId);
+    if (!tab.url.startsWith('http')) {
+        return;
+    }
+    await chrome.scripting.executeScript({
+        target: { tabId },
+        files: ['content.js'],
+    });
+}
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'checkInputFields') {
-        console.log('input count: ' + message.inputFieldCount)
         // Handle the message from the content script, like opening a sidepanel
         if (message.inputFieldCount > 0) {
             chrome.sidePanel.open({ tabId: sender.tab.id })
